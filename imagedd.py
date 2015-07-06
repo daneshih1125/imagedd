24d3b3fe87e8b0b89b4e6294c59c36c85a172d9b
dane <daneshih1125@gmail.com>
2015-07-06 11:45:15 +0800

    1. input 0 to dd all partitions 2. fixed parsing partition title fail

--- imagedd.py
+++ imagedd.py
@@ -1,20 +1,28 @@
-#!/usr/bin/python
-import commands
-import os
-import sys
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+import os, sys, re, commands
+
+dd_unit = 1
 
 def get_image_partitions(image):
     if not os.path.isfile(image): 
         return None 
-    FDISK_COL = 'Device                                                       Boot   Start     End Sectors  Size Id Type'
     fdisk_res = commands.getoutput("fdisk -l " + image)
-    fdisk_lines = fdisk_res.split('\n')
-    try:
-        start_idx = fdisk_lines.index(FDISK_COL) + 1
-    except:
+    idx = fdisk_res.find("Device")
+
+    if idx < 0:
         print "no partition"
         sys.exit(1)
-    partitions = [x for x in fdisk_lines[start_idx:] if x != '' ]
+
+    fdisk_res = fdisk_res[idx:]
+    fdisk_lines = fdisk_res.split('\n')
+
+    if (re.search("Blocks", fdisk_lines[0])):
+        global dd_unit 
+        dd_unit = 2
+
+    partitions = [x for x in fdisk_lines[1:] if x != '' ]
     return partitions
 
 def print_partitions(partitions):
@@ -25,21 +33,23 @@ def print_partitions(partitions):
 def cmd_dd(dd_list): 
     for dd_info in dd_list:
         item = dd_info.split()
-        print(item)
         dd_name = item[0]
         dd_skip = item[1]
-        dd_count = item[3]
-        cmd = "dd if=%s of=%s bs=512 skip=%s count=%s" % (sys.argv[1], dd_name, dd_skip, dd_count)
+        dd_count = int(item[3]) * dd_unit
+        cmd = "dd if=%s of=%s bs=512 skip=%s count=%d" % (sys.argv[1], dd_name, dd_skip, dd_count)
         print(cmd)
         os.system(cmd)
     
 def dd_partition(partitions):
     print_partitions(partitions)
     dd_no = int(raw_input('Input:'))
-    cmd_dd([partitions[dd_no-1]]);
+    if dd_no == 0:
+        cmd_dd(partitions)
+    elif dd_no < len(partitions):
+        cmd_dd([partitions[dd_no-1]])
+    else:
+        print("incorrect partition number")
 
 if __name__ == '__main__':
     image_partitions = get_image_partitions(sys.argv[1])
-    #print_partitions(image_partitions)
     dd_partition(image_partitions)
-    #print(image_partitions)
